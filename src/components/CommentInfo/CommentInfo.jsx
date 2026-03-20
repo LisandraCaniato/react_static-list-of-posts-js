cadf12692242ea31c0d721c5ac8fdfc91adce3cd
Lisandra <lisandracaniato@yahoo.com>
2026-03-20 12:19:47 -0300

    fix styles lint

--- src/components/CommentInfo/CommentInfo.jsx
+++ src/components/CommentInfo/CommentInfo.jsx
@@ -1,3 +1,5 @@
+import './CommentInfo.scss';
+
 export const CommentInfo = ({ comment }) => {
   const { name, email, body } = comment;
 
@@ -8,17 +10,12 @@ export const CommentInfo = ({ comment }) => {
 
         {' by '}
 
-        <a
-          className="CommentInfo__email"
-          href={`mailto:${email}`}
-        >
+        <a className="CommentInfo__email" href={`mailto:${email}`}>
           {email}
         </a>
       </div>
 
-      <div className="CommentInfo__body">
-        {body}
-      </div>
+      <div className="CommentInfo__body">{body}</div>
     </div>
   );
 };
